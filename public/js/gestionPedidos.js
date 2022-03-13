dcd7813305b5901f1b51f1aafd053826e9f333d7
Rafael Andres Granados <rafael.granados@gradiweb.com>
2022-03-12 22:08:09 -0500

    avances web service2

--- public/js/gestionPedidos.js
+++ public/js/gestionPedidos.js
@@ -5,3 +5,7 @@ const pedidosInstance = new Pedidos()
 $("#btn-nuevo-pedido").click(function(){
     pedidosInstance.abreModalPedidos()
 });
+
+$("#btn-ver-cobertura").click(function(){
+    pedidosInstance.consultaCobertura({usuario:'retabares.ws',clave:'c04dbbaa14d2c5600ff7f2ac6de2d5ae161bf1cb5a7df20ee7050db5bae5a945'})
+});
